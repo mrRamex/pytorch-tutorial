37d4cd019ffdf4d8523b4b9aa803cc7207046d1f
MrKrypex0 <rasmus@the-lighthouse.se>
2024-07-13 23:35:48 +0200

    Done with the basic Pytorch tutorial

---
+++ project-files/savenn.py
@@ -0,0 +1,9 @@
+import torch
+import torchvision.models as models
+
+model = models.vgg16(weights='IMAGENET1K_V1')
+torch.save(model.state_dict(), 'model_weights.pth')
+
+model = models.vgg16() # we do not specify ``weights``, i.e. create untrained model
+model.load_state_dict(torch.load('model_weights.pth'))
+print(model.eval())
